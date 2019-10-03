85e1ac6690c06e3c725e6827292ab496034c450b
jasonweiyi <jasonweiyi@gmail.com>
2019-10-03 15:35:14 +0100

    Added scripts.

---
+++ prior.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+
+from utils import multiple_formatter
+from functools import partial
+
+
+def kernel(x1: float, x2: float, l: float, sigma: float) -> float:
+    result = sigma**2 * np.exp(-0.5 * (x1-x2)**2 / (l**2))
+    return result
+
+
+def m(xs: np.ndarray) -> np.ndarray:
+    """
+    Zero mean function.
+    :param xs: the X locations.
+    :return: the mean vector for those locations.
+    """
+    return np.zeros(xs.shape)
+
+
+def km(xs1: np.ndarray, xs2: np.ndarray, l: float, sigma: float) -> np.ndarray:
+    assert len(list(xs1.shape)) == 1
+    assert len(list(xs2.shape)) == 1
+    result = np.zeros(shape=(xs1.shape[0], xs2.shape[0]))
+    for row_id, x1 in enumerate(xs1):
+        for col_id, x2 in enumerate(xs2):
+            result[row_id, col_id] = kernel(x1, x2, l, sigma)
+    return result
+
+
+#print(kernel(x1=1, x2=2, l=2, sigma=1))
+
+n = 50
+
+X = np.linspace(0, 2*np.pi, n)
+Y = np.sin(X)
+noise = np.random.normal(0, 0.1, n)
+Y += noise
+
+
+colors = ['red', 'blue']
+for index, l in enumerate([np.pi/100, np.pi/2]):
+
+    sigma = 1
+
+    k = partial(km, l=l, sigma=sigma)
+
+    # plt.scatter(X, Y)
+    # plt.show()
+
+
+    samples = []
+    probs = []
+    sample_number = 1
+    for i in range(sample_number):
+        sample = np.random.multivariate_normal(m(X), k(X, X))
+        samples.append(sample)
+
+        prob = multivariate_normal.pdf(
+            sample, mean=m(X), cov=k(X, X) + np.eye(n) * 1e-6,
+            allow_singular=False)
+        probs.append(prob)
+
+        # plt.plot(X, sample)
+
+
+    probs = np.array(probs)
+    normalized_prob = probs - np.min(probs)
+    normalized_prob /= np.max(normalized_prob)
+
+    print(normalized_prob)
+
+    for prob, sample in zip(normalized_prob, samples):
+        # alpha = prob * 10
+        alpha = 1
+        plt.plot(X, sample, alpha=alpha, markersize=5, marker='o', color=colors[index], label='l={}π'.format(0.01 if index==0 else 0.5))
+
+
+
+
+ax = plt.gca()
+ax.legend()
+ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 12))
+ax.xaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
+
+plt.show()
+
+# xs = np.array([1, 2, 3, 4])
+# xs1 = np.array([1, 2, 3])
+# xs2 = np.array([1, 2, 3, 4])
+# print(kernel_matrix(xs1, xs2, l=2, sigma=1))
+
---
+++ utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pylab as plt
+from scipy.stats import multivariate_normal
+
+
+# From https://stackoverflow.com/questions/40642061/how-to-set-axis-ticks-in-multiples-of-pi-python-matplotlib
+
+def multiple_formatter(denominator=2, number=np.pi, latex='\pi'):
+    def gcd(a, b):
+        while b:
+            a, b = b, a%b
+        return a
+    def _multiple_formatter(x, pos):
+        den = denominator
+        num = np.int(np.rint(den*x/number))
+        com = gcd(num,den)
+        (num,den) = (int(num/com),int(den/com))
+        if den==1:
+            if num==0:
+                return r'$0$'
+            if num==1:
+                return r'$%s$'%latex
+            elif num==-1:
+                return r'$-%s$'%latex
+            else:
+                return r'$%s%s$'%(num,latex)
+        else:
+            if num==1:
+                return r'$\frac{%s}{%s}$'%(latex,den)
+            elif num==-1:
+                return r'$\frac{-%s}{%s}$'%(latex,den)
+            else:
+                return r'$\frac{%s%s}{%s}$'%(num,latex,den)
+    return _multiple_formatter
+
+
+class Multiple:
+    def __init__(self, denominator=2, number=np.pi, latex='\pi'):
+        self.denominator = denominator
+        self.number = number
+        self.latex = latex
+
+    def locator(self):
+        return plt.MultipleLocator(self.number / self.denominator)
+
+    def formatter(self):
+        return plt.FuncFormatter(multiple_formatter(self.denominator, self.number, self.latex))
